aa7b8018c86d409d97106af6fec94f2d695200b1
Fatema Islam <1234+chad@users.noreply.github.com>
2020-01-21 16:25:41 -0500

    added a new method called greeting

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello{
+
+public static void main(String[] args){
+ Sytem.out.println("vs code is awesome..");   
+}
+greeting("Fatema boss");
+public static void greeting(String name){
+    Sytem.out.println("welcome " + name);   
+}
+
+}
